802307f37cb48c37b0b51e8227911ea503188680
yan-dmc <yancaio.dmc@gmail.com>
2025-02-26 16:53:40 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./style.css"
+import { HashRouter, Route, Routes } from "react-router-dom";
+import "./style.css";
 import { Game } from "./pages/Game";
 import { Menu } from "./pages/Menu";
 
 const App = () => {
   return (
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Menu />}/>
-            <Route path='/game-start' element={<Game />}/>
-            <Route path='*' element={<h1>Not Found</h1>}/>
-        </Routes>
-    </BrowserRouter>
-  )
-}
-
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/game-start" element={<Game />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </HashRouter>
+  );
+};
 
 export default App;
